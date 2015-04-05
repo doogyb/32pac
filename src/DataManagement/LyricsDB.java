e430a6bfc699d4b0896d70adbd98956dfc6a17a7
Zaid Al-Quraishi <zaid@isecur1ty.org>
2015-04-05 21:02:44 +0100

    getHtmlElement method, return element from URL

--- src/DataManagement/LyricsDB.java
+++ src/DataManagement/LyricsDB.java
@@ -2,6 +2,7 @@ package DataManagement;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -9,7 +10,6 @@ import java.net.URL;
 /**
  * Created by z00z on 04/04/15.
  */
- 
 public class LyricsDB {
     private static final String dbURL = "http://ohhla.com/favorite.html";
     private String url;
@@ -18,11 +18,14 @@ public class LyricsDB {
         url = inputURL;
     }
 
-    
-
+    private Elements getHtmlElement(String url ,String element_name) {
+        Elements element = null;
+        try {element = Jsoup.connect(url).get().select(element_name);}
+        catch (IOException e) {System.err.println("Caught IOException: " + e.getMessage());}
+        return element;
+    }
 
     public static void main(String[] args) {
-
 
     }
 }
