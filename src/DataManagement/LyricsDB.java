4ceca5898a63c64877acc7333d0adfbd87484e73
Zaid Al-Quraishi <zaid@isecur1ty.org>
2015-04-05 21:00:07 +0100

    initial commit, downloader class

---
+++ src/DataManagement/LyricsDB.java
@@ -0,0 +1,18 @@
+package DataManagement;
+
+/**
+ * Created by z00z on 04/04/15.
+ */
+ 
+public class LyricsDB {
+    private String url;
+
+    private LyricsDB(String inputURL) {
+        url = inputURL;
+    }
+
+  
+    public static void main(String[] args) {
+
+    }
+}
