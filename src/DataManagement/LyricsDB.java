c64b58ff147456954681b6870977bd8451592b56
Zaid Al-Quraishi <zaid@isecur1ty.org>
2015-04-05 21:01:36 +0100

    constructor

--- src/DataManagement/LyricsDB.java
+++ src/DataManagement/LyricsDB.java
@@ -1,18 +1,28 @@
 package DataManagement;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * Created by z00z on 04/04/15.
  */
  
 public class LyricsDB {
+    private static final String dbURL = "http://ohhla.com/favorite.html";
     private String url;
 
     private LyricsDB(String inputURL) {
         url = inputURL;
     }
 
-  
+    
+
+
     public static void main(String[] args) {
+
 
     }
 }
