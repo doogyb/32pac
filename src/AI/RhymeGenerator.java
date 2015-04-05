f1d0479b12f12b3faa1e4c78200cac2e2a5b4507
Zaid Al-Quraishi <zaid@isecur1ty.org>
2015-04-06 00:49:36 +0100

    getRhymes method reading words

--- src/AI/RhymeGenerator.java
+++ src/AI/RhymeGenerator.java
@@ -1,5 +1,10 @@
 package AI;
 import org.jsoup.Jsoup;
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by z00z on 06/04/15.
@@ -8,5 +13,22 @@ import org.jsoup.Jsoup;
 public class RhymeGenerator {
     private RhymeGenerator() {}
 
+    private Elements getRhymes(String word) {
+        Elements element = null;
+        try {
+            element = Jsoup.connect("http://muse.dillfrog.com/sound/search?match_type=perfect_rhyme&group_by=syllable_count&word_type=&word=" + word + "&familiar_only=Y&defined_only=Y&pb=%21").get().select("div div");
+            try {
+                System.out.println(element.get(1).select("a").text());
+                System.out.println(element.get(3).select("a").text());
+                System.out.println(element.get(5).select("a").text());
+            } catch (IndexOutOfBoundsException e) {}
+        }
+        catch (IOException e) {System.err.println("Caught IOException: " + e.getMessage());}
+        return element;
+    }
     
+    public static void main(String[] args) {
+        RhymeGenerator r = new RhymeGenerator();
+        Elements e = r.getRhymes("zaid");
+    }
 }
