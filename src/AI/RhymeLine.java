ca0a578d7705bcc058c36648274b7d23f0ba9ac7
doogyballs <samueldoogan@gmail.com>
2015-04-25 17:23:46 +0100

    committing before pull

--- src/AI/RhymeLine.java
+++ src/AI/RhymeLine.java
@@ -4,14 +4,16 @@ package AI;
  * Created by doogy on 09/03/15.
  */
 public class RhymeLine {
-    String line1, line2, artist, album, songName, syllables;
+    protected String line1, line2, artist, album, songName, lastWord;
+    protected int  syllables, score=0;
     public RhymeLine(String l1, String l2, String ar, String al, String song, String syl) {
         line1 = l1;
         line2 = l2;
         artist = ar;
         album = al;
         songName = song;
-        syllables=syl;
+        syllables=Integer.parseInt(syl.substring(1));
+        lastWord = NaturalLanguage.getLastWord(l2);
     }
     public String toString() {
         return "Artist: " + artist +"\nAlbum" + album + "Song: " + songName +"\n\n Lines: \n" + line1 + "\n" + line2;
