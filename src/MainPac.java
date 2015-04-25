d5a6a4e8360ca3d1e8bfc78f7f7b399c7552cffc
Zaid Al-Quraishi <zaid@isecur1ty.org>
2015-04-25 14:40:59 +0100

    updateLyrics method and check to download lyrics

--- src/MainPac.java
+++ src/MainPac.java
@@ -1,13 +1,38 @@
 import AI.NaturalLanguage;
+import DataManagement.LyricsDB;
 import TwitterInteraction.TwitterActions;
+
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by doogy on 17/04/15.
  */
 
 public class MainPac {
+    private static final String dbURL = "http://ohhla.com/favorite.html";
 
-    public static void main (String[] args) {
+    private static boolean updateLyrics() {
+        System.out.println("Do you want to update your lyrics database ? (y/n) ");
+        while (true) {
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                String input = br.readLine();
+                if (input.equals("y") || input.equals("yes")) return true;
+                else if (input.equals("n" )|| input.equals("no")) return false;
+                else System.out.println("[-] Incorrect input, please enter y/n");
+            } catch (IOException e ) {System.out.println("[-] Incorrect input.");return false;}
+        }
+    }
+
+    public static void main (String[] args) throws IOException {
+        if (updateLyrics() || !LyricsDB.haveLyrics()) {
+            System.out.println("[+] Checking for updates ... ");
+            LyricsDB db = new LyricsDB(dbURL);
+            db.downloadSongs();
+        }
         TwitterActions TA = new TwitterActions();
         TA.authorization();
         TA.listener();
