635a75e2757b13e21e134cb4a90b1ecba37d753b
Zaid Al-Quraishi <zaid@isecur1ty.org>
2015-04-25 17:53:19 +0100

    Logic modification, replaced getLastword with getLastTweetWord

--- src/AI/NaturalLanguage.java
+++ src/AI/NaturalLanguage.java
@@ -2,6 +2,7 @@ package AI;
 
 import org.jsoup.Jsoup;
 import org.jsoup.select.Elements;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -9,7 +10,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by samuel on 16/04/15.
@@ -17,131 +21,80 @@ import java.util.Set;
 public class NaturalLanguage {
 
 	static Set<String> dict = genDict();
-
+			
 	public static HashSet<String> genDict() {
 		HashSet<String> dict = new HashSet<String>();
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("unix.dict"));
 			String line;
-			while ((line=br.readLine())!=null){
+			while ((line=br.readLine())!=null)
 				if (line.length() > 3) dict.add(line);
-			}
-			br.close();
 		} catch (IOException e) { e.printStackTrace(); }
-		return dict;
+        return dict;
 	}
 
-	//    public static String removeLastWords(String input) {
-	//        String lastWord = NaturalLanguage.getLastWord(input);
-	//
-	//        while ( lastWord.charAt(0) == '@' || lastWord.charAt(0) == '#' || lastWord.contains("http://t.co") || lastWord.contains("https://t.co")) {
-	//            input = input.substring(0, input.length() - lastWord.length()-1);
-	//            lastWord = NaturalLanguage.getLastWord(input);
-	//        }
-	//        String last="";
-	//        int i;
-	//        for (i = lastWord.toCharArray().length - 1; i >= 0; i--) {
-	//            if (Character.isLetterOrDigit(lastWord.charAt(i)) && lastWord.charAt(i)!=' ')
-	//                break;
-	//        }
-	//
-	//        return input.substring(0,input.length()-lastWord.length()-1) + lastWord.substring(0,i+1);
-	//    }
 
-	public static String removeLastWords(String input) {
-		String[] in = input.split(" ");
-		Boolean miss=false;
-		for (int i = in.length - 1; i >= 0; i--) {
-			for (int j=0; j<in[i].length()-1; j++) {
-				if (!Character.isLetter(in[i].charAt(j))) {
-					miss = true;
-					break;
-				}
-			}
-			if (!miss) {
-				if (!Character.isLetter((in[i].charAt(in[i].length()-1))))
-					in[i]=in[i].substring(0,in[i].length()-1);
-				String out="";
-				for (int k=0; k<=i;k++)
-					out+=in[k] + " ";
-				return out;
-			}
-			miss=false;
-		}
-		return null;
-	}
+    public static String getLastTweetWord(String input) {
+        String inputArray[] = input.split(" ");
+        int i;
 
-	public static int numberOfSyllables(String input) {
-		int syllablesCount = 0;
-		char[] word = input.toCharArray();
+        for (i = inputArray.length-1; i >= 0; i--) {
+            String currentWord = inputArray[i].substring(0, inputArray[i].length()-1);
+            System.out.println("processing " + currentWord);
+            Pattern wordPattern = Pattern.compile("[^a-z0-9]", Pattern.CASE_INSENSITIVE);
+            Matcher m = wordPattern.matcher(currentWord);
 
-		for (int i = 0; i < word.length-1; i++) {
-			if (isVowel(word[i])) {
-				syllablesCount++;
-				while (isVowel(word[i])) i++;
-			}
-		}
+            if (!m.find()) break;
+        }
+        return inputArray[i];
+    }
 
-		return syllablesCount;
-	}
+    public static int numberOfSyllables(String input) {
+        int syllablesCount = 0;
+        char[] word = input.toCharArray();
 
-	public static boolean isVowel(char c) {
-		return (c == 'e' || c == 'a' || c == 'i' || c == 'o' || c == 'u' || c == 'y');
-	}
+        for (int i = 0; i < word.length-1; i++) {
+            if (isVowel(word[i])) {
+                syllablesCount++;
+                while (isVowel(word[i])) i++;
+            }
+        }
 
-	public static String getLastWord(String input) {
-		String[] out = input.split(" ");
-		if (out.length<1) return null;
-		return out[out.length-1];
-	}
+        return syllablesCount;
+    }
 
-	public static HashMap<Integer, ArrayList<String>> getRhymes(String word) {
-		Elements element = null;
-		HashMap<Integer, ArrayList<String>> rhymes = new HashMap<Integer, ArrayList<String>>();
-		rhymes.put(1,new ArrayList<String>());
-		rhymes.put(2,new ArrayList<String>());
-		rhymes.put(3,new ArrayList<String>());
+    public static boolean isVowel(char c) {
+        return (c == 'e' || c == 'a' || c == 'i' || c == 'o' || c == 'u' || c == 'y');
+    }
 
-		try {
-			element = Jsoup.connect("http://muse.dillfrog.com/sound/search?match_type=perfect_rhyme&group_by=syllable_count&word_type=&word=" + word + "&familiar_only=Y&defined_only=Y&pb=%21").get().select("div div");
-			try {
-				rhymes.get(1).addAll(Arrays.asList(element.get(1).select("a").text().split(" ")));
-				rhymes.get(2).addAll(Arrays.asList(element.get(3).select("a").text().split(" ")));
-				rhymes.get(3).addAll(Arrays.asList(element.get(5).select("a").text().split(" ")));
-			} catch (IndexOutOfBoundsException e) {}
-		}
-		catch (IOException e) {System.err.println("Caught IOException: " + e.getMessage());}
-		return rhymes;
-	}
+    public static String getLastWord(String input) {
+        String[] out = input.split(" ");
+        if (out.length<1) return null;
+        return out[out.length-1];
+    }
 
-	public static String filter(String rhyme){
-		byte[][] words = {new byte[]{78, 105, 103, 103, 101, 114}, new byte[]{110, 105, 103, 103, 101, 114},
-						  new byte[]{78, 105, 103, 103, 97}, new byte[]{110, 105, 103, 103, 97},
-						  new byte[]{80, 117, 115, 115, 121}, new byte[]{112, 117, 115, 115, 121},
-						  new byte[]{70, 117, 99, 107}, new byte[]{102, 117, 99, 107},
-						  new byte[]{83, 104, 105, 116}, new byte[]{115, 104, 105, 116},
-						  new byte[]{68, 105, 99, 107}, new byte[]{100, 105, 99, 107}
-		};
+    public static HashMap<Integer, ArrayList<String>> getRhymes(String word) {
+        Elements element = null;
+        HashMap<Integer, ArrayList<String>> rhymes = new HashMap<Integer, ArrayList<String>>();
+        rhymes.put(1,new ArrayList<String>());
+        rhymes.put(2,new ArrayList<String>());
+        rhymes.put(3,new ArrayList<String>());
 
-		byte[][] filter = {new byte[]{78, 105, 42, 42, 101, 114}, new byte[]{110, 105, 42, 42, 101, 114},
-						   new byte[]{78, 105, 42, 42, 97}, new byte[]{110, 105, 42, 42, 97},
-						   new byte[]{80, 117, 42, 42, 121}, new byte[]{112, 117, 42, 42, 121},
-						   new byte[]{70, 42, 42, 107}, new byte[]{102, 42, 42, 107},
-						   new byte[]{83, 42, 42, 116}, new byte[]{115, 42, 42, 116},
-						   new byte[]{68, 42, 99, 107}, new byte[]{100, 42, 99, 107}
-		};
-		for (int i = 0; i < 12; i++){
-			String x = new String(words[i]);
-			if (rhyme.contains(x)){
-				rhyme = rhyme.replace(x, new String(filter[i]));
-			}
-		}
-		return rhyme;
-	}
-
-	public static ArrayList<String> splitString(String input) {
+        try {
+            element = Jsoup.connect("http://muse.dillfrog.com/sound/search?match_type=perfect_rhyme&group_by=syllable_count&word_type=&word=" + word + "&familiar_only=Y&defined_only=Y&pb=%21").get().select("div div");
+            try {
+                rhymes.get(1).addAll(Arrays.asList(element.get(1).select("a").text().split(" ")));
+                rhymes.get(2).addAll(Arrays.asList(element.get(3).select("a").text().split(" ")));
+                rhymes.get(3).addAll(Arrays.asList(element.get(5).select("a").text().split(" ")));
+            } catch (IndexOutOfBoundsException e) {}
+        }
+        catch (IOException e) {System.err.println("Caught IOException: " + e.getMessage());}
+        return rhymes;
+    }
+    
+    public static ArrayList<String> splitString(String input) {
 		ArrayList<String> out = new ArrayList<String>();
-		int pos = 0, matched = 0;
+    	int pos = 0, matched = 0;
 		String match = "";
 		if (input.length() < 3) return null;
 		if (dict.contains(input)) return null;
